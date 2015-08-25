f5d13b12cd1ec4b687e75b4241634ea83030f22f
root <root@localhost.localdomain>
2015-08-25 16:02:48 +0800

    Initial commit

---
+++ app/src/main/java/com/zhangyuzhou/searchincomingcall/MainActivity.java
@@ -0,0 +1,61 @@
+package com.zhangyuzhou.searchincomingcall;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String searchEngine = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //RadioGroup contraller to choose which searth engine to use
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch (view.getId()) {
+            case R.id.googleButton:
+                if (checked)
+                    searchEngine = "www.google.com.hk/search?q=";
+                break;
+            case R.id.baiduButton:
+                if (checked)
+                    searchEngine = "www.baidu.com/s?wd=";
+                break;
+        }
+        //Log.d("Search engine is: ", searchEngine);
+    }
+}
